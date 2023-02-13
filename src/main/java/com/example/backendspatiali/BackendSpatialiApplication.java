82d876d010d1df0a43c4208bb732a0f0d6ea0c62
araraya <aryadanihlesmana@gmail.com>
2023-02-13 13:21:56 +0700

    initilize commit

---
+++ src/main/java/com/example/backendspatiali/BackendSpatialiApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendspatiali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpatialiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpatialiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendspatiali/BackendSpatialiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendspatiali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpatialiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
