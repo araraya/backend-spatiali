915b932d68e73bec161b52443df52262b7ef239c
araraya <aryadanihlesmana@gmail.com>
2023-02-14 14:38:18 +0700

    create authentication controller

---
+++ src/main/java/com/example/backendspatiali/authentication/controller/AuthenticationController.java
@@ -0,0 +1,49 @@
+package com.example.backendspatiali.authentication.controller;
+
+
+import com.example.backendspatiali.authentication.data.AuthenticationResponse;
+import com.example.backendspatiali.authentication.data.LoginRequest;
+import com.example.backendspatiali.authentication.data.RegisterRequest;
+import com.example.backendspatiali.authentication.service.AuthenticationService;
+import com.example.backendspatiali.user.User;
+import com.example.backendspatiali.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final UserRepository userRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+        Optional<User> user = userRepository.findByUsername(request.getUsername());
+        if(user.toString() == "Optional.empty"){
+            return ResponseEntity.ok(authenticationService.register(request));
+        } else {
+            final AuthenticationResponse response = AuthenticationResponse.builder()
+                    .status("Error, User already exist")
+                    .token("null")
+                    .build();
+            return ResponseEntity.ok(response);
+        }
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody LoginRequest request
+    ){
+        return ResponseEntity.ok(authenticationService.login(request));
+    }
+}
